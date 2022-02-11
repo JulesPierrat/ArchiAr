4d9ffa29cbcb1c3124bbe9b442719b80924f44c2
JulesPierrat <jules.pierrat@ensg.eu>
2022-02-11 10:06:52 +0100

    Init

---
+++ myApp/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import MyComponent from 'xeokit-viewer-react-library'
+import 'xeokit-viewer-react-library/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
